24b65cbc7f4601e212c06a021b6945b4fca5aa0d
dhruvalgolakiya <dhruval@golakiya.com>
2022-11-16 15:31:21 +0530

    driveapp commity

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/google", indexRouter);
 app.use("/users", usersRouter);
-
+app.get('/',(req,res)=>{
+  res.render('homepage')
+})
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
