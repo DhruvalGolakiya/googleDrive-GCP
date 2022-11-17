53d1b18a95dd07663014066774d92fe26df5f7a0
dhruvalgolakiya <dhruval@golakiya.com>
2022-11-17 15:38:27 +0530

    cloudbuild config

--- config/config.js
+++ config/config.js
@@ -1,13 +1,12 @@
 const env = process.env;
-let _REDIRECT_URI = "";
+let REDIRECT_URI = "";
 let ORIGIN_URI =
   env.ORIGIN_URI || "https://peaceful-rex-368804.uc.r.appspot.com";
 
 if (ORIGIN_URI == env.ORIGIN_URI) {
-  _REDIRECT_URI = env.REDIRECT_URI;
+  REDIRECT_URI = env.REDIRECT_URI;
 } else {
-  _REDIRECT_URI =
-    "https://peaceful-rex-368804.uc.r.appspot.com/google/callback";
+  REDIRECT_URI = "https://peaceful-rex-368804.uc.r.appspot.com/google/callback";
 }
 
- module.exports = { _REDIRECT_URI };
+module.exports = { REDIRECT_URI };
